ae818da5f0acfc189e7292fcd3da23173686f162
Kaufmann Beda Maurice <kab618@troesch.group>
2017-10-18 10:21:37 +0200

    gitignore

---
+++ IT-Projekt_dominion/src/Dominion/Client/ClientClasses/test.java
@@ -0,0 +1,25 @@
+package Dominion.Client.ClientClasses;
+
+
+
+import com.sun.xml.internal.fastinfoset.util.StringArray;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
+
+public class test {
+	private static String workingDirectory = System.getProperty("user.dir");
+        public static void main(String[] args) {
+        	
+        	//System.out.println(dbClass.getInstance().userFileExists());
+            //System.out.println(workingDirectory);
+            //System.out.println(dbClass.getInstance().pwCorrect("jada","2222"));
+            System.out.println("jada"+System.getProperty("line.separator")+"jada");
+
+        }
+    }
+
