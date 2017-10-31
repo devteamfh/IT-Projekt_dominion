fe7031918a66e702bf75b41c7f356551b64bbc21
joel henz <joel.henz@students.fhnw.ch>
2017-10-31 18:58:40 +0100

    alles ok

--- IT-Projekt_dominion/src/Dominion/Client/Client.java
+++ IT-Projekt_dominion/src/Dominion/Client/Client.java
@@ -1,17 +1,8 @@
 package Dominion.Client;
 
-import java.net.InetAddress;
-
-import Dominion.Client.ClientClasses.Client_Controller_lobby;
 import Dominion.Client.ClientClasses.Client_Controller_start;
 import Dominion.Client.ClientClasses.Client_Model;
-import Dominion.Client.ClientClasses.Client_View_lobby;
 import Dominion.Client.ClientClasses.Client_View_start;
-import Dominion.Client.ClientClasses.ServiceLocatorClient;
-import Dominion.appClasses.GameObject;
-import Dominion.appClasses.GameObject.ObjectType;
-import Dominion.appClasses.Player;
-import Dominion.appClasses.StartInformation;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -21,7 +12,6 @@ import javafx.stage.Stage;
 public class Client extends Application {
 	
 	private Client_View_start view;
-	private ServiceLocatorClient sl = ServiceLocatorClient.getServiceLocator();
 	
 	public static void main(String[] args) {		
 		launch (args);
@@ -35,36 +25,6 @@ public class Client extends Application {
         new Client_Controller_start(model, view);
 
         view.start();
-        
-        /**InetAddress addr = InetAddress.getByName("127.0.0.1");
-		int portNr = 8000;
-    	//String name = view.tf_userName.getText();
-    	//model.setName(name);
-		
-		String name = "joel";
-    	model.setName(name);
-		
-		model.connectToServer(addr,portNr);	
-		
-		if(sl.getConnected()){
-			model.setPlayer();
-			StartInformation current = new StartInformation(model.getName());
-			current.setID();
-			
-			GameObject obj= new GameObject (GameObject.ObjectType.StartInformation);
-			obj.setID();
-			
-			obj = current;
-			
-			model.getOutput().writeObject(obj);
-			model.getOutput().flush();
-		}
-    	
-    	Stage playingStage = new Stage();				
-        Client_View_lobby view2 = new Client_View_lobby(playingStage, model);
-        new Client_Controller_lobby(model, view2); 
-        view2.start();
-        view.stop();*/
        
 	}
 	
---
+++ IT-Projekt_dominion/src/Dominion/Client/ClientClasses/customButton.java
@@ -0,0 +1,115 @@
+package Dominion.Client.ClientClasses;
+
+import java.util.ArrayList;
+
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.input.MouseEvent;
+
+public class customButton extends Button {
+	private customButton btn;
+	private Label lbl;
+	private String lblText;
+	
+	//constructor
+	public customButton() {
+		super();
+	}
+	
+	public customButton(String s){
+		super(s);
+	}
+	
+	
+	public void setBtnTextEmpty(customButton btn){
+	this.btn=btn;
+	this.lblText = this.btn.getText();
+	this.lbl = new Label(this.lblText);
+	this.btn.setText("");    
+    this.lbl.getStyleClass().add("lbl");
+    this.btn.setGraphic(this.lbl);
+	
+	}
+	
+	
+	
+	
+	
+	//TextEffekt dem Button hinzufügen
+	/*id setLblTextEffect(customButton btn){
+		this.btn = btn;
+		this.lbl = (Label) this.btn.getGraphic();
+	    lbl.setStyle("-fx-effect: dropshadow( one-pass-box , white , 1 , 0.0 , 1 , 0 )");		
+	}
+*/
+	
+	
+	
+	/*public void addStyleHandlerClickListener(){
+		
+		addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+			@Override public void handle(MouseEvent e) {
+		/*		if (getStyleClass().contains("clicked"))	{
+
+				getStyleClass().add("button");
+				
+				}else {
+				getStyleClass().add("clicked");
+						}
+				}
+		});
+		
+	}*/
+
+	/*
+	public void setStyleClicked(){
+		getStyleClass().add("clicked");
+		
+	}
+	
+	public void setStyleUnclicked(){
+		getStyleClass().clear();
+		getStyleClass().add("button");
+	}
+	
+	
+	public void setStyleBtnAdtnl(){
+		getStyleClass().add("btn_adtnl");
+
+	}
+	*/
+	
+	
+	/*	public void addStyleHandlerDropShadow(){
+		DropShadow shadow = new DropShadow();
+		
+		//Adding the shadow when the mouse cursor is on
+		addEventHandler(MouseEvent.MOUSE_ENTERED, 
+		    new EventHandler<MouseEvent>() {
+		        @Override public void handle(MouseEvent e) {
+		        
+		        }
+		});
+		
+		//Removing the shadow when the mouse cursor is off
+		addEventHandler(MouseEvent.MOUSE_EXITED, 
+		    new EventHandler<MouseEvent>() {
+		        @Override public void handle(MouseEvent e) {
+		        	
+		        }
+		});
+		}
+		*/
+	
+		//Label 
+	@Override
+		public String toString(){
+			String labelText = this.lblText;
+			return labelText;
+			}
+		
+		
+
+}
