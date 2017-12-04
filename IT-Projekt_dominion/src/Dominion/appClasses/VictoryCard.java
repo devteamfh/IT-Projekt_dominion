cf6134f458a8e42a1aba1b55569c4f4dc9c70231
stogge5000 <32070278+stogge5000@users.noreply.github.com>
2017-12-05 00:23:59 +0100

    1. Teil VictoryCard
    Ist nur ein Ansatz... läuft leider nicht wie gewollt lokal, deshalb lade ich nur einen Teil hoch.. sorry..

--- IT-Projekt_dominion/src/Dominion/appClasses/VictoryCard.java
+++ IT-Projekt_dominion/src/Dominion/appClasses/VictoryCard.java
@@ -1,27 +1,138 @@
 package Dominion.appClasses;
 
-import javafx.scene.image.Image;
+import java.util.ArrayList;
+
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 
 /**
  * @author David Steuri
  *
  */
 
-public class VictoryCard {
+//Switch Statement für verschiedene Karten
+
+public class VictoryCard extends Button {
+	private VictoryCard vbtn;
+	private Label vlbl;
+	private String vlblText;
+	public VictoryCard(VictoryCardType vbtn) {
+		super();
+	}
+	public VictoryCard(String s) {
+		super(s);
+	}
+
+	public enum VictoryCardType {
+		Estate(1, 2, "Anwesen", new ImageIcon("/IT-Projekt_dominion/Cards/Cards/Punkte/Punkte_01.jpg")),
+		Duchy(3, 5, "Herzogtum", new ImageIcon("/IT-Projekt_dominion/Cards/Cards/Punkte/Punkte_03.jpg")),
+		Province(6, 8, "Provinz", new ImageIcon("/IT-Projekt_dominion/Cards/Cards/Punkte/Punkte_06.jpg"));
+
 	private int victoryPoints;
 	private int costs;
-	private int sizeOfStack;
 	private String title;
-	private Image victoryImg;
+	private ImageIcon image;
 
-		
-// Konstruktor für VictoryCard?
-public VictoryCard(int VictoryPoints, int costs, int sizeOfStack, String title, Image victoryImg){
-	this.victoryPoints = VictoryPoints;
+	
+	VictoryCardType(int victoryPoints, int costs, String title, ImageIcon image){
+	this.victoryPoints = victoryPoints;
 	this.costs = costs;
-	this.sizeOfStack = 10; // Muss auf dem Tisch 10 sein. Was passiert wenn gekauft?
 	this.title = title;
-	this.victoryImg = victoryImg;
+	this.image = image;
+	
+	}
+	// Getter und Setter für VictoryCard-Objekte
+	public int getVictoryPoints(){
+		return this.victoryPoints;	
+		}
+	
+	public int getCosts(){
+		return this.costs;
+	}
+	
+	public String getTitle(){
+		return this.title;
+	}
+	
+	public ImageIcon getImage(){
+		return this.image;
+	}
+	
+	
+	// Spiel wird gestartet, Stapel für Estate
+	public void setVictoryCardStackEstate(){
+			VictoryCard [] stackEstate = new VictoryCard[10];
+			for (int i=0; i < stackEstate.length; i++){
+			stackEstate[i] = new VictoryCard(Estate);
+			}
+		}
+	// Spiel wird gestartet, Stapel für Duchy
+	public void setVictoryCardStackDuchy(){
+			VictoryCard [] stackDuchy = new VictoryCard[10];
+			for (int i=0; i < stackDuchy.length; i++){
+			stackDuchy[i] = new VictoryCard(Duchy);
+			}
+		}
+	// Spiel wird gestartet, Stapel für Province
+	public void setVictoryCardStackProvince(){
+			VictoryCard [] stackProvince = new VictoryCard[10];
+			for (int i=0; i < stackProvince.length; i++){
+			stackProvince[i] = new VictoryCard(Province);
+			}
+		}
+
+	}
+	}
+	
+
+
+/*
+	public void setBtnVictoryCard(VictoryCard vbtn){
+	this.vbtn=vbtn; 
+    this.vlbl.getStyleClass().add("vlbl");
+    this.vbtn.setGraphic(this.vlbl);
+	}
+
+	@Override
+	public String toString(){
+		String labelText = this.vlblText;
+		return labelText;
+		}
+}
+
+
+
+
+/*
+   private VictoryCardType(int victoryPoints, int costs, String title, Image imgViewEstate){
+    	this.victoryPoints = victoryPoints;
+    	this.costs = costs;
+    	this.title = title;
+    	this.victoryImg = victoryImg;
+    	
+    }
+    
+   
+   
+		public void setVictoryPoints () {
+        switch (victoryCard) {
+        case Estate:
+        	public int getVictoryPoints(){
+        		return (int) points = victoryPoints+1;
+        	}
+        	
+        default:
+            public int getVictoryPoints(){
+            	return 0;
+            }
+            break;
+        }
+	
 	
 	// Stimmt das so?
 }
@@ -84,3 +195,5 @@ public void trash(VictoryCard v){
 
 
 }
+
+ */
