ba70a8296df2a3152862dc873202071a36d998af
aw7000 <32979629+aw7000@users.noreply.github.com>
2017-12-05 16:24:41 +0100

    ActionCard & TrasureCard
    Zum besprechen, komme ohne zusammenfügen nicht mehr weiter. Constructor, Buy und Play funktioniert in der Rohfassung.

--- IT-Projekt_dominion/src/Dominion/appClasses/TreasureCard.java
+++ IT-Projekt_dominion/src/Dominion/appClasses/TreasureCard.java
@@ -1,90 +1,139 @@
-package Dominion.appClasses;					// TODO mit Testklasse (Klon von Client_View_playingStage) bis 9.12.2017
+package Dominion.appClasses;
+
+import java.awt.Button;
+import javafx.scene.image.Image;
 
 /**
  * @author Adrian Widmer
  */
 
-public class TreasureCard //extends Button 		// TODO Button importieren?
+public class TreasureCard extends Button 		
 {
-	
 	private String title;					
-	private String description;				
-											// TODO Image als Variable?
+	private Image treasureImg;
 	private int costs;						
-	private int counter;					// TODO Was wird gez�hlt? Anzahl auf der Hand?
-	private int trasureValue;				
-	private int sizeOfStack;				// Was wenn karte auf der Hand und nicht auf Deck? =0?
+	private int counter;					// TODO Was wird gezaehlt? Anzahl auf der Hand?
+	private int treasureValue;				
+	private int sizeOfStack;				// Was wenn Karte auf der Hand und nicht auf Deck? =0?
+	private TreasureCardType type;
 	
+	public enum TreasureCardType				
+	{
+		Kupfer, Silber, Gold;
+	}
+	
+	//constructor
+	public TreasureCard(TreasureCardType type) {
+		super();
+		this.type = type;
+		switch(type){
+        case Kupfer:
+        	this.title = "Kupfer";
+    		//this.treasureImg = ;					// TODO Pfad der Datei?
+    		this.costs = 0;
+    		this.treasureValue = 1; 
+    		//this.counter = 0;						// TODO Siehe Frage oben
+    		//this.sizeOfStack = sizeOfStack;		// TODO Initial 0?
+    		break;
+        case Silber:
+        	this.title = "Silber";
+    		//this.treasureImg = ;					// TODO Pfad der Datei?
+    		this.costs = 3;
+    		this.treasureValue = 2; 
+    		//this.counter = 0;						// TODO Siehe Frage oben
+    		//this.sizeOfStack = sizeOfStack;		// TODO Initial 0?
+    		break;
+        case Gold:
+        	this.title = "Gold";
+    		//this.treasureImg = ;					// TODO Pfad der Datei?
+    		this.costs = 6;
+    		this.treasureValue = 3; 
+    		//this.counter = 0;						// TODO Siehe Frage oben
+    		//this.sizeOfStack = sizeOfStack;		// TODO Initial 0?
+    		break;
+       default:
+            System.out.println("Karte konnte nicht erzeugt werden.");
+        } 
+	}
+
+	//getter & setter
 	public String getTitle() {
 		return title;
 	}
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	public String getDescription() {
-		return description;
+
+	public Image getTreasureImg() {
+		return treasureImg;
 	}
-	public void setDescription(String description) {
-		this.description = description;
+
+	public void setTreasureImg(Image treasureImg) {
+		this.treasureImg = treasureImg;
 	}
+	
 	public int getCosts() {
 		return costs;
 	}
+
 	public void setCosts(int costs) {
 		this.costs = costs;
 	}
+
 	public int getCounter() {
-		return counter;
+		return counter;	
 	}
+
 	public void setCounter(int counter) {
 		this.counter = counter;
 	}
+
 	public int getTrasureValue() {
-		return trasureValue;
+		return treasureValue;
 	}
+
 	public void setTrasureValue(int trasureValue) {
-		this.trasureValue = trasureValue;
+		this.treasureValue = trasureValue;
 	}
+
 	public int getSizeOfStack() {
 		return sizeOfStack;
 	}
+
 	public void setSizeOfStack(int sizeOfStack) {
 		this.sizeOfStack = sizeOfStack;
 	}
 
-	public TreasureCard()					// TODO Verschiedene Konstruktoren notwendig? Hand oder Deck?
-	{
+	public TreasureCardType getType() {
+		return type;
+	}
+
+	public void setType(TreasureCardType type) {
+		this.type = type;
+	}
+
+	// Move card to DiscardPile
+	public void throwOnDiscardPile()
+		{
+		// TODO Karte nach LinkedList discardPile verschieben
+		}
 	
+	// Buy card
+	public void buy()
+		{
+		// TODO analog ActionCard
+		}
+	
+	// Trash card
+	public void trashCard()				
+	{
+		// TODO Wo soll Karte hin? nur deleten?
 	}
 	
-	public void throwOnDiscardPile()		// TODO
+	//Play card
+	public void playXy ()		
 	{
-	/* 1. Karte zu eingesetzte Karten verschieben
-	 * 2. Karte aus Hand entfernen
-	 */
-	}
-	
-	public void buy()						// TODO
-	/* Vorbedingung: Kaufbare Karten sind aktiviert, nicht kaufbare Karten sind deaktiviert
-	 * 1. Abfragen costs der Deckkarte 
-	 * 2. Abgleichen mit treasure Karten auf der Hand
-	 * 3. Treasurekarten auf der hand abziehen
-	 * 4. Actioncard auf gekaufte karten erg�nzen
-	 */
-	{
-		
-	}
-	
-	public void trashCard()				// TODO
-	{
-		//Wo soll Karte hin?
-	}
-		
-	public void playXy ()		// TODO --> Pro Enum eine Methode?
-	{
-	/* 1. Switch Statement --> https://docs.oracle.com/javase/tutorial/java/nutsandbolts/switch.html
-	 * 	
-	 */
+		// TODO --> TreasureKarten sind nicht klickbar, sondern werden nur im Total angezeigt (numberOfTreasures) --> Vereinfacht Buy-Methode bei ActionCard
 	}
 	
 }
