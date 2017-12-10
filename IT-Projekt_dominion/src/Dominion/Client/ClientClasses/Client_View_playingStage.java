9065734bde4ff75d41016bc06237b263938c8f71
kab <b.kaufmann@students.fhnw.ch>
2017-12-10 13:17:12 +0100

    middlecards

--- IT-Projekt_dominion/src/Dominion/Client/ClientClasses/Client_View_playingStage.java
+++ IT-Projekt_dominion/src/Dominion/Client/ClientClasses/Client_View_playingStage.java
@@ -56,6 +56,11 @@ public class Client_View_playingStage extends View<Client_Model> implements Obse
 	
 	ArrayList<GameCard> al_communityCards_left;
 	
+	
+	
+	
+	
+	
 	Label buyPower;
 	Label actions;
 	Label buys;
@@ -83,8 +88,7 @@ public class Client_View_playingStage extends View<Client_Model> implements Obse
     
     HBox tryUpdateshit;
     
-	HBox hb_wrp_communityActionCardsBackRow;
-	HBox hb_wrp_communityActionCardsFrontRow;
+
     
 
 	public Client_View_playingStage(Stage stage, Client_Model model, GameParty party) {
@@ -98,7 +102,11 @@ public class Client_View_playingStage extends View<Client_Model> implements Obse
 	protected Scene create_GUI() {
 		croupier = Croupier.getCroupier();
 		
+	    sl = ServiceLocatorClient.getServiceLocator();  
 		
+		BorderPane root = new BorderPane();
+
+		//_______________X Button top right__________________________//
 		btn_close = new Button();
 		btn_close.setPrefSize(35, 33);
 	   	btn_close.getStyleClass().addAll("btn","btn_close_normal");
@@ -116,10 +124,8 @@ public class Client_View_playingStage extends View<Client_Model> implements Obse
 
 		
 		
-		
-		
-
-		//____Community Cards links -> Lï¿½ndereien und Geld_____________________________________________//
+	
+		//____Community Cards links -> Lï¿½ndereien und Geld, Curse____________________________________________//
 		estate   = new ProvinceCard(new Label("estate"),croupier.getCostsEstate());
 		duchy    = new ProvinceCard(new Label("duchy"),croupier.getCostsDuchy());
 		province = new ProvinceCard(new Label("province"),croupier.getCostsPovince());
@@ -145,18 +151,101 @@ public class Client_View_playingStage extends View<Client_Model> implements Obse
 		for (int i = 0; i < al_communityCards_left.size(); i++) {
 			gc = al_communityCards_left.get(i);
 			croupier.addObserver(gc);
-			gc.setMinSize(110, 120);
+			gc.setMinSize(120, 110);
 		}
-		//--------------------------------------------------------------------------------------------//
 		
+		//Branches
+		HBox hb_wrapper_communityCards_Left = new HBox();
+		hb_wrapper_communityCards_Left.setPadding(new Insets(0,20,20,20));
+		
+		VBox vb_wrapper_communityCards_Left_col1 = new VBox();
+		VBox vb_wrapper_communityCards_Left_col2 = new VBox();
+		 
+		HBox hb_wrapper_province = new HBox();   hb_wrapper_province.getChildren().add(province);  hb_wrapper_province.setPadding(new Insets(0,5,5,0));
+		HBox hb_wrapper_duchy    = new HBox();   hb_wrapper_duchy.getChildren().add(duchy); 	   hb_wrapper_duchy.setPadding(new Insets(0,5,5,0));
+		HBox hb_wrapper_estate   = new HBox();   hb_wrapper_estate.getChildren().add(estate);  	   hb_wrapper_estate.setPadding(new Insets(0,5,5,0));
+		HBox hb_wrapper_curse    = new HBox();   hb_wrapper_curse.getChildren().add(curse);	  	   hb_wrapper_curse.setPadding(new Insets(0,5,5,0)); 	
+		HBox hb_wrapper_gold     = new HBox();   hb_wrapper_gold.getChildren().add(gold);	  	   hb_wrapper_gold.setPadding(new Insets(0,5,5,0));
+		HBox hb_wrapper_silver   = new HBox();   hb_wrapper_silver.getChildren().add(silver);      hb_wrapper_silver.setPadding(new Insets(0,5,5,0));
+		HBox hb_wrapper_copper   = new HBox();   hb_wrapper_copper.getChildren().add(copper);      hb_wrapper_copper.setPadding(new Insets(0,5,5,0));		
+		
+		vb_wrapper_communityCards_Left_col1.getChildren().addAll(hb_wrapper_province,hb_wrapper_duchy,hb_wrapper_estate,hb_wrapper_curse);
+		vb_wrapper_communityCards_Left_col2.getChildren().addAll(hb_wrapper_gold, hb_wrapper_silver, hb_wrapper_copper);
+
+		
+		hb_wrapper_communityCards_Left.getChildren().addAll(vb_wrapper_communityCards_Left_col1, vb_wrapper_communityCards_Left_col2);
+		
+		//--------------------------------------------------------------------------------------------------------------------------------------//
+		
+		
+		
+		
+		//____________Community Action Cards (Mitte)___________________________________________________________________________________________________//
 		
 		//Allen CommunityActionCards einen observer hinzufügen
 		for (GameCard ac : croupier.getAl_communityActionCards()){
 		croupier.addObserver(ac);	
 		}
 				
-		//community Action Cards in der Mitte mit  10 karten initialisieren
+		//community Action Cards in der Mitte mit 10 karten initialisieren
 		croupier.prepareAL_stackSizeCommunityActionCards();
+		
+		for (int i = 0;i<10;i++){
+			croupier.getAl_communityActionCards().get(i).setMinSize(200, 170);
+			}
+		
+		
+		//Branches
+		VBox vb_wrapper_center = new VBox();
+		vb_wrapper_center.setMinSize(1200, 600);
+		vb_wrapper_center.setPadding(new Insets(0,50,0,50));
+		
+		HBox hb_wrp_communityActionCardsBackRow   = new HBox();
+		hb_wrp_communityActionCardsBackRow.setPadding(new Insets(30,0,0,0));
+		
+		HBox hb_wrp_communityActionCardsFrontRow  = new HBox();
+		
+		HBox hb_wrapper_comntyACSlot0 = new HBox();   hb_wrapper_comntyACSlot0.getChildren().add(croupier.getAl_communityActionCards().get(0));  hb_wrapper_comntyACSlot0.setPadding(new Insets(0,5,5,0));
+		HBox hb_wrapper_comntyACSlot1 = new HBox();   hb_wrapper_comntyACSlot1.getChildren().add(croupier.getAl_communityActionCards().get(1));  hb_wrapper_comntyACSlot1.setPadding(new Insets(0,5,5,0));
+		HBox hb_wrapper_comntyACSlot2 = new HBox();   hb_wrapper_comntyACSlot2.getChildren().add(croupier.getAl_communityActionCards().get(2));  hb_wrapper_comntyACSlot2.setPadding(new Insets(0,5,5,0));
+		HBox hb_wrapper_comntyACSlot3 = new HBox();   hb_wrapper_comntyACSlot3.getChildren().add(croupier.getAl_communityActionCards().get(3));  hb_wrapper_comntyACSlot3.setPadding(new Insets(0,5,5,0));
+		HBox hb_wrapper_comntyACSlot4 = new HBox();   hb_wrapper_comntyACSlot4.getChildren().add(croupier.getAl_communityActionCards().get(4));  hb_wrapper_comntyACSlot4.setPadding(new Insets(0,5,5,0));
+		HBox hb_wrapper_comntyACSlot5 = new HBox();   hb_wrapper_comntyACSlot5.getChildren().add(croupier.getAl_communityActionCards().get(5));  hb_wrapper_comntyACSlot5.setPadding(new Insets(0,5,5,0));
+		HBox hb_wrapper_comntyACSlot6 = new HBox();   hb_wrapper_comntyACSlot6.getChildren().add(croupier.getAl_communityActionCards().get(6));  hb_wrapper_comntyACSlot6.setPadding(new Insets(0,5,5,0));
+		HBox hb_wrapper_comntyACSlot7 = new HBox();   hb_wrapper_comntyACSlot7.getChildren().add(croupier.getAl_communityActionCards().get(7));  hb_wrapper_comntyACSlot7.setPadding(new Insets(0,5,5,0));
+		HBox hb_wrapper_comntyACSlot8 = new HBox();   hb_wrapper_comntyACSlot8.getChildren().add(croupier.getAl_communityActionCards().get(8));  hb_wrapper_comntyACSlot8.setPadding(new Insets(0,5,5,0));
+		HBox hb_wrapper_comntyACSlot9 = new HBox();   hb_wrapper_comntyACSlot9.getChildren().add(croupier.getAl_communityActionCards().get(9));  hb_wrapper_comntyACSlot9.setPadding(new Insets(0,5,5,0));
+		
+		hb_wrp_communityActionCardsBackRow.getChildren().addAll(hb_wrapper_comntyACSlot0,hb_wrapper_comntyACSlot1,hb_wrapper_comntyACSlot2,hb_wrapper_comntyACSlot3,hb_wrapper_comntyACSlot4);
+		hb_wrp_communityActionCardsFrontRow.getChildren().addAll(hb_wrapper_comntyACSlot5,hb_wrapper_comntyACSlot6,hb_wrapper_comntyACSlot7,hb_wrapper_comntyACSlot8,hb_wrapper_comntyACSlot9);
+
+		vb_wrapper_center.getChildren().addAll(hb_wrp_communityActionCardsBackRow,hb_wrp_communityActionCardsFrontRow);
+		
+		
+	
+		//--------------------------------------------------------------------------------------------------------------------------------------//
+		
+		
+		
+		
+		
+		
+		
+		//____________Community Action Cards (Mitte)___________________________________________________________________________________________________//
+				//--------------------------------------------------------------------------------------------------------------------------------------//
+				
+		
+		//____________Community Action Cards (Mitte)___________________________________________________________________________________________________//
+				//--------------------------------------------------------------------------------------------------------------------------------------//
+				
+		//____________Community Action Cards (Mitte)___________________________________________________________________________________________________//
+				//--------------------------------------------------------------------------------------------------------------------------------------//
+				
+		
+		
+		
+		gold.addClickListener();
+
 		
 		
 
@@ -177,7 +266,7 @@ public class Client_View_playingStage extends View<Client_Model> implements Obse
 		
 		
 		
-	    sl = ServiceLocatorClient.getServiceLocator();  
+
 	    
 	    chatWindowPlayingStage = sl.getTextAreaChatPlayingStage();
 		chatWindowPlayingStage.setEditable(false);
@@ -245,7 +334,7 @@ public class Client_View_playingStage extends View<Client_Model> implements Obse
 		
 		vb_right.getChildren().addAll(label_gameHistory,windowGameHistory,label_chat,hb_chatButtonAndTextField,chatWindowPlayingStage);
 		
-		BorderPane root = new BorderPane();
+
 		
 		vb_player = new VBox();
 		
@@ -261,16 +350,15 @@ public class Client_View_playingStage extends View<Client_Model> implements Obse
 		vb_player.setPrefHeight(60.0);
 		
 		//setting the treasure and point cards to buy
-		GridPane gp_left = new GridPane();
+		//GridPane gp_left = new GridPane();
 		
 
 		//Button curse = new Button ("Curse");
-		
-		gold.addClickListener();
+
 		//Button silver = new Button ("Silver");
 		//Button copper = new Button ("Copper");
 		
-		GridPane.setConstraints(province, 0, 0);
+	/*	GridPane.setConstraints(province, 0, 0);
 		GridPane.setConstraints(duchy, 0, 1);
 		GridPane.setConstraints(estate, 0, 2);
 		//GridPane.setConstraints(curse, 0, 3);
@@ -280,6 +368,8 @@ public class Client_View_playingStage extends View<Client_Model> implements Obse
 		GridPane.setConstraints(copper, 1, 2);
 		
 		gp_left.getChildren().addAll(province,duchy,estate,gold,silver,copper);
+		*/
+		
 		
 		VBox vb_center = new VBox();
 		//GridPane gp_actionCards = new GridPane();
@@ -290,24 +380,7 @@ public class Client_View_playingStage extends View<Client_Model> implements Obse
 		
 		
 		
-		 hb_wrp_communityActionCardsBackRow = new HBox();
-		 hb_wrp_communityActionCardsFrontRow = new HBox();
 		
-		
-		hb_wrp_communityActionCardsBackRow.getChildren().addAll(croupier.getAl_communityActionCards().get(0),croupier.getAl_communityActionCards().get(1),croupier.getAl_communityActionCards().get(2),croupier.getAl_communityActionCards().get(3),croupier.getAl_communityActionCards().get(4));
-		hb_wrp_communityActionCardsFrontRow.getChildren().addAll(croupier.getAl_communityActionCards().get(5),croupier.getAl_communityActionCards().get(6),croupier.getAl_communityActionCards().get(7),croupier.getAl_communityActionCards().get(8),croupier.getAl_communityActionCards().get(9));
-		
-	
-		for (int i = 0;i<10;i++){
-			croupier.getAl_communityActionCards().get(i).setPrefSize(300,200);
-			}
-		
-		
-		//gc1.setMinSize(100,200);
-		
-
-		
-		vb_center.getChildren().addAll(hb_wrp_communityActionCardsBackRow,hb_wrp_communityActionCardsFrontRow);
 		
 		Label playedCards_label = new Label ("gespielte Karten");
 		vb_center.getChildren().addAll(playedCards_label);
@@ -356,9 +429,9 @@ public class Client_View_playingStage extends View<Client_Model> implements Obse
 		vb_bottom.setAlignment(Pos.TOP_CENTER);
 		
 		root.setTop(hb_custom_menue);
-		root.setLeft(gp_left);
-		BorderPane.setMargin(gp_left, new Insets(0, 20, 0, 0));
-		root.setCenter(vb_center);
+		root.setLeft(hb_wrapper_communityCards_Left);
+		//BorderPane.setMargin(gp_left, new Insets(0, 20, 0, 0));
+		root.setCenter(vb_wrapper_center);
 		root.setRight(vb_right);
 		root.setBottom(vb_bottom);
 		
@@ -370,7 +443,7 @@ public class Client_View_playingStage extends View<Client_Model> implements Obse
 			sl.getButtonLeaveGamePlayer().setDisable(false);
 		}
 		
-		this.scene = new Scene (root,1000,800);
+		this.scene = new Scene (root,1750,800);
 		scene.getStylesheets().add(getClass().getResource("/stylesheets/style_playStage.css").toExternalForm());
 	    //stage.initStyle(StageStyle.TRANSPARENT);   
 		
@@ -381,6 +454,29 @@ public class Client_View_playingStage extends View<Client_Model> implements Obse
 		        public void handle(MouseEvent mouseEvent) {
 		            System.out.println("mouse click detected! " + mouseEvent.getSource());
 		            croupier.setSimpleIntegerPropertyBuyPower();
+		            
+		            //redraw community cards left
+		            hb_wrapper_province.getChildren().remove(province);  
+		    		hb_wrapper_duchy.getChildren().remove(duchy); 	     
+		    		hb_wrapper_estate.getChildren().remove(estate);  	  
+		    		hb_wrapper_curse.getChildren().remove(curse);	  	 
+		    		hb_wrapper_gold.getChildren().remove(gold);	  	  
+		    		hb_wrapper_silver.getChildren().remove(silver);     
+		    		hb_wrapper_copper.getChildren().remove(copper); 
+		    		
+		            hb_wrapper_province.getChildren().add(province);  
+		    		hb_wrapper_duchy.getChildren().add(duchy); 	     
+		    		hb_wrapper_estate.getChildren().add(estate);  	  
+		    		hb_wrapper_curse.getChildren().add(curse);	  	 
+		    		hb_wrapper_gold.getChildren().add(gold);	  	  
+		    		hb_wrapper_silver.getChildren().add(silver);     
+		    		hb_wrapper_copper.getChildren().add(copper);    
+		            
+
+		    		
+
+		            
+		            
 		        	tryUpdateshit.getChildren().clear();
 		        	tryUpdateshit.getChildren().addAll(croupier.getLbltest());
 		        }
